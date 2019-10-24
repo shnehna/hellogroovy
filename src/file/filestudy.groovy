021ca33a1d4e8dcefef89afd3689b63cd23e41f6
shenhan <mr_shenhan@outlook.com>
2019-10-24 10:39:59 +0800

    文件的读写操作

---
+++ src/file/filestudy.groovy
@@ -0,0 +1,23 @@
+package file
+
+/**
+ * @author shenhan* @create 2019/10/24 10:18
+ * @description
+ */
+def file = new File('../../hellogroovy.iml')
+/*file.eachLine {
+    line -> println line
+}*/
+
+def text = file.getText()
+//println text
+
+def result = file.readLines()
+//println result
+
+def reader = file.withReader { reader ->
+    char[] buffer = new char[100]
+    reader.read(buffer)
+    return buffer
+}
+println reader
